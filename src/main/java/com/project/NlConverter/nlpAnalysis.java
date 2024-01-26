f65da93dcaee6ccf5cca237111dbd900efb43304
Cianna MacMahon <ciannamacmahon@gmail.com>
2024-01-26 13:49:50 +0000

    cleaned up code

--- src/main/java/com/project/NlConverter/nlpAnalysis.java
+++ src/main/java/com/project/NlConverter/nlpAnalysis.java
@@ -15,7 +15,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class nlpAnalysis {
-    public static LinkedList<String> qualities = new LinkedList<>();
+    public static LinkedList<String> depQualities = new LinkedList<>();
     public static LinkedList<String[]> quest_ans_targets=new LinkedList<>();
     public static String question_entity="";
     public static String subject="";
@@ -29,12 +29,9 @@ public class nlpAnalysis {
         String question = "Where was Obama born?";
         nlpAnalysis.init();
         languageAnalysis(question);
-        // populateTargetQuestions();
-        //   nlpAnalysis.findPOStags(question);
-       // predictTarget();
-        //       qualities = nlpAnalysis.dependancyParser(question);
-        //nlpAnalysis.dependancyParser(question);
-        // nlpAnalysis.pringQual(qualities);
+        populateTargetQuestions();
+        predictTarget();
+        nlpAnalysis.pringQual(depQualities);
     }
 
 
@@ -43,7 +40,7 @@ public class nlpAnalysis {
         Scanner scanner= new Scanner(file);
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            System.out.println(line);
+          //  System.out.println(line);
             String[] parts = line.split(", ");
 
             // Add the array to the list
@@ -54,7 +51,7 @@ public class nlpAnalysis {
 
     public static void predictTarget(){
         for (String[] q:quest_ans_targets){
-            System.out.println(questionWord+"hihiih"+q[0]);
+           // System.out.println(questionWord+"hihiih"+q[0]);
             if(q[0].equals(questionWord)){
                 targetAnswer=q[1];
             }
@@ -65,7 +62,7 @@ public class nlpAnalysis {
         else if (questionWord.equals("When")||questionWord.equals("Where")){
             unknown="object";
         }
-        System.out.println(targetAnswer+" "+ unknown);
+        System.out.println("Target Answer: "+targetAnswer+" Word type: "+unknown);
     }
     public static void SPOExtraction(LinkedList<String> qualOfQuery) {
         LinkedList<String[]> listOfArray = intoArray(qualOfQuery);
@@ -117,21 +114,28 @@ public class nlpAnalysis {
         LinkedList<String[]> divArrays = new LinkedList<>();
 
         for (String parser : qualOfQuery) {
-            System.out.println(parser);
+            // each line of the depParser is printed, split into an array of words to enable SPO extraction
+          //  System.out.println(parser);
             divArrays.add(splitString(parser));
         }
         return divArrays;
     }
     public static String[] splitString(String input) {
-        System.out.println(input);
         String word_type="";
         String[] result = new String[3];
+        //Result with split the sentence into an array of 3 so SPO extraction can take place
+        //Result=[wordType][associated word][evaluated word]
+        //Eg:    [punct][born][?]
 
+
+        // Sentence: aux:pass(born-4, was-2)
         word_type = input.substring(0, input.indexOf("("));
-        System.out.println(word_type);
+        // aux:pass
+       // System.out.println(word_type);
         result[0]=word_type;
         String subtext=input.substring(input.indexOf("(")+1,input.indexOf(")"));
-        System.out.println(subtext);
+        //Subtext: born-4, was-2
+      //  System.out.println(subtext);
 
         // Split the string using ',' as the delimiter
         String[] parts = subtext.split(", ");
@@ -166,8 +170,8 @@ public class nlpAnalysis {
         pipeline.annotate(document);
         findPOStags(document);
         findNER(document);
-        constituencyParse(document,query);
-        dependancyParser(document,query);
+        constituencyParse(document);
+        depQualities=dependancyParser(query);
     }
 
     public static void findPOStags(CoreDocument doc){
@@ -179,10 +183,6 @@ public class nlpAnalysis {
                 questionWord=token.word();
             }
         }
-        //Method 2:
-        List<String>posTags=sentence.posTags();
-        System.out.println("POS tags");
-        System.out.println(posTags);
     }
     public static void findNER(CoreDocument doc){
         System.out.println("---");
@@ -203,7 +203,7 @@ public class nlpAnalysis {
 
     }
 
-    public static void constituencyParse(CoreDocument doc,String query){
+    public static void constituencyParse(CoreDocument doc){
         CoreSentence sentence=doc.sentences().get(0);
         //Method 1:
         Tree constitParse1 = sentence.constituencyParse();
@@ -211,47 +211,23 @@ public class nlpAnalysis {
         System.out.println(constitParse1);
         System.out.println();
 
-        //Method 2:
-        Annotation docu=new Annotation(query);
-        pipeline.annotate(docu);
-        CoreMap sent=docu.get(CoreAnnotations.SentencesAnnotation.class).get(0);
-
-        Tree constitParse2 = sent.get(TreeCoreAnnotations.TreeAnnotation.class);
-        System.out.println("Constituency Parse:");
-        System.out.println(constitParse2);
     }
     @SuppressWarnings("unchecked")
-    //    public static LinkedList<String> dependancyParser(String q1)
-    public static void dependancyParser(CoreDocument document, String query)
+    public static LinkedList<String> dependancyParser(String query)
     {
         LinkedList<String> depParse= new LinkedList<>();
-       // Method 1:
-        CoreSentence sentence1=document.sentences().get(0);
-        // dependency parse for the second sentence
-        SemanticGraph dependencyParse = sentence1.dependencyParse();
-        System.out.println("Example: dependency parse");
-        System.out.println(dependencyParse);
-        System.out.println();
-
         //Method 2:
         Annotation doc=new Annotation(query);
         pipeline.annotate(doc);
         CoreMap sentence=doc.get(CoreAnnotations.SentencesAnnotation.class).get(0);
-
         SemanticGraph depGraph =sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+        //Gets the dependancy Parse
+        System.out.println("Depenecy parse");
+        for (TypedDependency typedDependency : depGraph.typedDependencies()) {
+            String line = typedDependency.toString();
+            depParse.add(line);
 
-            //Gets the dependancy Parse
-
-            System.out.println("Depenecy parse");
-            for (TypedDependency typedDependency : depGraph.typedDependencies()) {
-                String line = typedDependency.toString();
-                System.out.println(line);
-                //depParse.add(line);
-
-            }
-
-
-
-        //return depParse;
+        }
+        return depParse;
     }
 }
